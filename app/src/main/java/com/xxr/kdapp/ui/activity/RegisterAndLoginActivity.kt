0b8d93607ac597593afb14363315dde309e836e4
许夏荣 <624591992@qq.com>
2020-04-17 21:13:26 +0800

    提交注册页代码

---
+++ app/src/main/java/com/xxr/kdapp/ui/activity/RegisterAndLoginActivity.kt
@@ -0,0 +1,20 @@
+package com.xxr.kdapp.ui.activity
+
+import com.xxr.kdapp.R
+import com.xxr.kdapp.base.BaseActivity
+
+class RegisterAndLoginActivity : BaseActivity() {
+    override fun layoutId(): Int {
+        return R.layout.activity_register_and_login
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+
+}
